cb5216e9382f1937120fffaf8b333d0deb2f8162
titiscms <titisbackup@gmail.com>
2020-08-17 20:39:29 -0300

    Customizando os endpoints de GrupoPermissao e inserindo na aba Grupos na documentação do swagger

---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/GrupoPermissaoControllerOpenApi.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.PermissaoDTO;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Grupos")
+public interface GrupoPermissaoControllerOpenApi {
+
+	@ApiOperation("Listar as permissões associadas a um grupo")
+    @ApiResponses({
+        @ApiResponse(code = 400, message = "ID do grupo inválido", response = Problem.class),
+        @ApiResponse(code = 404, message = "Grupo não encontrado", response = Problem.class)
+    })
+	public List<PermissaoDTO> listar(
+			@ApiParam(value = "ID do grupo", example = "1", required = true)
+			Long grupoId);
+	
+	@ApiOperation("Associação de permissão com grupo")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Permissão associada com sucesso"),
+		@ApiResponse(code = 404, message = "Grupo ou permissão não encontrado", response = Problem.class)
+	})
+	public void associar(
+			@ApiParam(value = "ID do grupo", example = "1", required = true)
+			Long grupoId,
+			@ApiParam(value = "ID da permissão", example = "2", required = true)
+			Long permissaoId);
+	
+	@ApiOperation("Desassociação de permissão com grupo")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Permissão desassociada com sucesso"),
+		@ApiResponse(code = 404, message = "Grupo ou permissão não encontrado", response = Problem.class)
+	})
+	public void desassociar(
+			@ApiParam(value = "ID do grupo", example = "1", required = true)
+			Long grupoId,
+			@ApiParam(value = "ID da permissão", example = "2", required = true)
+			Long permissaoId);
+}
