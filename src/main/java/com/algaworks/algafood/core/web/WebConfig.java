af4cd76a78b2b372d690419bb21e70a18b694406
titiscms <titisbackup@gmail.com>
2021-02-28 20:07:16 -0300

    Configurando a api usar a versão 2 quando o mediaType (Accept) não for informado.

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
 //		.allowedOrigins("*")
 //		.maxAge(30)
 		.allowedMethods("*");
+	}
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(AlgafoodMediaTypes.V2_APPLICATION_JSON);
 	}
 	
 	@Bean
