d1753113d0a1fd7efeec83855fe7b96ab3b32f6b
titiscms <titisbackup@gmail.com>
2021-04-11 22:01:08 -0300

    Implementando a restrição na api usando as permissões contidas no payload do jwt token

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.core.security;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +20,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().authenticated()
+				.antMatchers(HttpMethod.DELETE, "/v1/cozinha/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.POST, "/v1/cozinha/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.PUT, "/v1/cozinha/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.GET, "/v1/cozinha/**").authenticated()
+				.anyRequest().denyAll()
 		.and()
 			/*
 			 * configuração para não manter a sessão.
@@ -23,7 +33,30 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.cors()
 		.and()
-			.oauth2ResourceServer().jwt();
+			.oauth2ResourceServer()
+				.jwt()
+				/*
+				 * configuração para reconhecer as authorities no corpo do jwt token
+				 */
+				.jwtAuthenticationConverter(jwtAuthenticationConverter());
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			var authorities = jwt.getClaimAsStringList("authorities");
+			
+			if (authorities == null) {
+				authorities = Collections.emptyList();
+			}
+			
+			return authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		});
+		
+		
+		return jwtAuthenticationConverter;
 	}
 	
 	/*
